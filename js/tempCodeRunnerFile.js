8973322dd7a823e4ca3d65e37eb69e81d3430fac
Martin Žofčík <martinzof1@gmail.com>
2021-01-03 14:11:43 +0100

    final final final FINAL ODOVZDANA VERZIA

--- js/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-// make animal glow on mouseover
-            animal.on('mouseover', function () {
-              animal.image(images[privKey + '_glow']);
-              animalLayer.draw();
-              document.body.style.cursor = 'pointer';
-            });
